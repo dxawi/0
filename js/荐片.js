72eb568c6931a2dd8e62305c718b677b656aed3b
dxawi <107306227+dxawi@users.noreply.github.com>
2023-10-17 22:36:11 +0800

    Add files via upload

--- js/荐片.js
+++ js/荐片.js
@@ -1,9 +1,9 @@
 var rule = {
     title:'荐片',
-    host:'http://jpmobile.acdn.axcxa.top',
-   // host:'http://api2.rinhome.com',
-    homeUrl:'https://yjpapipxblwdohpakljwg.hxhzs.com/api/tag/hand?code=unknown601193cf375db73d&channel=wandoujia',//网站的首页链接,用于分类获取和推荐获取
-   // homeUrl:'/api/tag/hand?code=unknown601193cf375db73d&channel=wandoujia',//网站的首页链接,用于分类获取和推荐获取
+    //host:'http://jpmobile.acdn.axcxa.top',
+    host:'http://api2.rinhome.com',
+    //homeUrl:'https://yjpapipxblwdohpakljwg.hxhzs.com/api/tag/hand?code=unknown601193cf375db73d&channel=wandoujia',//网站的首页链接,用于分类获取和推荐获取
+    homeUrl:'/api/tag/hand?code=unknown601193cf375db73d&channel=wandoujia',//网站的首页链接,用于分类获取和推荐获取
     // url:'/api/crumb/list?area=0&category_id=fyclass&page=fypage&type=0&limit=24&fyfilter',
     url:'/api/crumb/list?area=0&page=fypage&type=0&limit=24&fyfilter',
     class_name:'电视剧&综艺&电影&动漫&全部',     // 筛选 /api/term/ad_fenlei?limit=10&page=1
